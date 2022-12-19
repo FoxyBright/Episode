963c3da7c3218f46babb91948cbf497de02c1bcb
Albert Gaitian <a.gaitian@cprogroup.ru>
2022-12-19 18:21:53 +0300

    CoroutineController

--- core/src/main/java/ru/rikmasters/gilty/core/data/repository/Repository.kt
+++ core/src/main/java/ru/rikmasters/gilty/core/data/repository/Repository.kt
@@ -2,12 +2,13 @@ package ru.rikmasters.gilty.core.data.repository
 
 import kotlinx.coroutines.*
 import ru.rikmasters.gilty.core.common.Component
+import ru.rikmasters.gilty.core.common.CoroutineController
 import ru.rikmasters.gilty.core.data.source.Source
 
 abstract class Repository<T: Source>(
     
     protected open val primarySource: T
     
-): Component {
+): CoroutineController(), Component {
 
 }
--- core/src/main/java/ru/rikmasters/gilty/core/viewmodel/ViewModel.kt
+++ core/src/main/java/ru/rikmasters/gilty/core/viewmodel/ViewModel.kt
@@ -1,17 +1,11 @@
 package ru.rikmasters.gilty.core.viewmodel
 
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.*
 import ru.rikmasters.gilty.core.common.Component
-import ru.rikmasters.gilty.core.util.inline.methodName
+import ru.rikmasters.gilty.core.common.CoroutineController
 
-abstract class ViewModel: Component {
-    
-    companion object {
-        val DEFAULT_SINGLE_STRATEGY = Strategy.CANCEL
-    }
-
-    protected val scope = CoroutineScope(SupervisorJob())
+abstract class ViewModel: CoroutineController(), Component {
     
     
     private val loadingMut = MutableStateFlow(false)
@@ -27,33 +21,10 @@ abstract class ViewModel: Component {
     }
     
     
-    protected val calls: MutableMap<String, Deferred<*>> = HashMap()
-    
     protected suspend inline fun <T> singleLoading(
         strategy: Strategy = DEFAULT_SINGLE_STRATEGY,
-        crossinline block: suspend CoroutineScope.() -> T
+        noinline block: suspend CoroutineScope.() -> T
     ): T = loading { single(strategy, block) }
-    
-    @Suppress("UNCHECKED_CAST")
-    protected suspend inline fun <T> single(
-        strategy: Strategy = DEFAULT_SINGLE_STRATEGY,
-        crossinline block: suspend CoroutineScope.() -> T
-    ): T {
-        val methodName = methodName()
-        calls[methodName]?.let {
-            when(strategy) {
-                Strategy.CANCEL -> it.cancel("Отмена одиночной задачи $methodName")
-                Strategy.THROW -> throw IllegalStateException("Одиночная задача $methodName уже начата")
-                Strategy.JOIN -> return it.await() as T
-            }
-        }
-        
-        val deferred = scope.async { block() }
-        calls[methodName] = deferred
-        val res = deferred.await()
-        calls.remove(methodName, deferred)
-        return res
-    }
     
     
     protected fun <T> Flow<T>.state(
