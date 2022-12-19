0caf19edd2afa6a6fe4c9ea6b498ada758c1610a
Albert Gaitian <a.gaitian@cprogroup.ru>
2022-12-19 12:00:17 +0300

    Отключение CI плагина

--- buildSrc/src/main/kotlin/CiAndroidPlugin.kt
+++ buildSrc/src/main/kotlin/CiAndroidPlugin.kt
@@ -4,7 +4,7 @@ import org.gradle.kotlin.dsl.apply
 class CiAndroidPlugin: CiPlugin() {
     
     override fun apply(target: Project) {
-        //if(env["CI_ACTIVE"].equals("true", true).not()) return
+        if(env["CI_ACTIVE"].equals("true", true).not()) return
         
         target.childProjects.values
             .forEach {
