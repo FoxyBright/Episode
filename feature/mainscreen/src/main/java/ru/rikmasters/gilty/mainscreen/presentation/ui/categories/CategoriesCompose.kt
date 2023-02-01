05cff81635211c7e9e7f57b8439c2e1da604e022
Ivan Evstratov <i.evstratov@rikmasters.ru>
2023-02-01 14:07:18 +0300

    Изменены категории в фильтрах встреч

--- feature/mainscreen/src/main/java/ru/rikmasters/gilty/mainscreen/presentation/ui/categories/CategoriesCompose.kt
+++ feature/mainscreen/src/main/java/ru/rikmasters/gilty/mainscreen/presentation/ui/categories/CategoriesCompose.kt
@@ -12,7 +12,9 @@ import androidx.compose.material.icons.Icons.Filled
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.*
+import androidx.compose.material3.CardDefaults.cardColors
 import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.CenterVertically
@@ -26,86 +28,159 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ru.rikmasters.gilty.mainscreen.presentation.ui.main.custom.FlowLayout
 import ru.rikmasters.gilty.shared.R
-import ru.rikmasters.gilty.shared.R.drawable.enabled_check_box
 import ru.rikmasters.gilty.shared.R.drawable.ic_back
 import ru.rikmasters.gilty.shared.model.meeting.CategoryModel
 import ru.rikmasters.gilty.shared.model.meeting.DemoCategoryModel
 import ru.rikmasters.gilty.shared.shared.*
 import ru.rikmasters.gilty.shared.theme.base.GiltyTheme
 
-@Preview(backgroundColor = 0xFFE8E8E8, showBackground = true)
+@Preview
 @Composable
 private fun CategoryListPreview() {
     GiltyTheme {
-        CategoryList(
-            CategoryListState(
-                listOf(DemoCategoryModel),
-                emptyList(), emptyList()
-            ), Modifier.padding(16.dp)
-        )
+        Box(
+            Modifier.background(
+                colorScheme.background
+            )
+        ) {
+            CategoryList(
+                CategoryListState(
+                    listOf(DemoCategoryModel),
+                    emptyList(), emptyList()
+                ), Modifier.padding(16.dp)
+            )
+        }
     }
 }
 
 interface CategoryListCallback {
     
-    fun onCategoryClick(index: Int, it: Boolean)
-    fun onSubSelect(category: CategoryModel, sub: String?) {}
-    fun onBack() {}
-    fun onDone() {}
-    fun onClear() {}
+    fun onCategoryClick(index: Int, category: CategoryModel)
+    fun onSubClick(category: CategoryModel)
+    fun onBack()
+    fun onComplete()
+    fun onClear()
 }
 
 data class CategoryListState(
-    val categoryList: List<CategoryModel>,
-    val categoryListState: List<Boolean>,
-    val subCategories: List<Pair<CategoryModel, String>>
+    val categories: List<CategoryModel>,
+    val selected: List<CategoryModel>,
+    val states: List<Int>,
 )
 
 @Composable
+@OptIn(ExperimentalMaterial3Api::class)
 fun CategoryList(
     state: CategoryListState,
     modifier: Modifier = Modifier,
-    callback: CategoryListCallback? = null
+    callback: CategoryListCallback? = null,
 ) {
-    Column(modifier) {
-        Row(
-            Modifier
-                .padding(bottom = 10.dp)
-                .offset((-6).dp),
-            Start, CenterVertically
-        ) {
-            IconButton(
-                { callback?.onBack() },
-            ) {
-                Icon(
-                    painterResource(ic_back),
-                    (null), Modifier,
-                    colorScheme.tertiary
-                )
-            }
-            Text(
-                stringResource(R.string.meeting_filter_select_categories),
-                Modifier.padding(start = 14.dp),
-                colorScheme.tertiary,
-                style = typography.labelLarge
+    Scaffold(modifier.padding(16.dp), {
+        TopBar { callback?.onBack() }
+    }, {
+        Buttons(
+            Modifier, { callback?.onComplete() })
+        { callback?.onClear() }
+    }) { Categories(Modifier.padding(it), state, callback) }
+}
+
+@Composable
+private fun TopBar(
+    onBack: () -> Unit,
+) {
+    Row(
+        Modifier
+            .padding(bottom = 10.dp)
+            .offset((-6).dp),
+        Start, CenterVertically
+    ) {
+        IconButton({ onBack() }) {
+            Icon(
+                painterResource(ic_back),
+                (null), Modifier,
+                colorScheme.tertiary
             )
         }
-        Column(Modifier.fillMaxSize(), SpaceBetween) {
-            LazyColumn(Modifier.weight(1f)) {
-                itemsIndexed(state.categoryList)
-                { index, category ->
-                    val select = state.categoryListState[index]
-                    Item(category, select, state.subCategories,
-                        { callback?.onSubSelect(category, it) })
-                    {
-                        callback?.onSubSelect(category, null)
-                        callback?.onCategoryClick(index, select)
+        Text(
+            stringResource(R.string.meeting_filter_select_categories),
+            Modifier.padding(start = 14.dp),
+            colorScheme.tertiary,
+            style = typography.labelLarge
+        )
+    }
+}
+
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun Categories(
+    modifier: Modifier = Modifier,
+    state: CategoryListState,
+    callback: CategoryListCallback? = null,
+) {
+    val states = state.states
+    LazyColumn(modifier) {
+        itemsIndexed(state.categories) { index, category ->
+            Card(
+                { callback?.onCategoryClick(index, category) },
+                Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 12.dp),
+                shape = shapes.large,
+                colors = cardColors(colorScheme.primaryContainer),
+            ) {
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                    SpaceBetween, CenterVertically
+                ) {
+                    Row(Modifier, Start, CenterVertically) {
+                        GEmojiImage(
+                            category.emoji,
+                            Modifier.size(20.dp)
+                        )
+                        Text(
+                            category.name,
+                            Modifier
+                                .padding(start = 18.dp)
+                                .padding(vertical = 16.dp),
+                            colorScheme.tertiary,
+                            style = typography.bodyMedium,
+                            fontWeight = SemiBold
+                        )
+                    }
+                    if(!category.children.isNullOrEmpty()) Icon(
+                        if(states.contains(index))
+                            Filled.KeyboardArrowDown
+                        else Filled.KeyboardArrowRight, (null),
+                        Modifier, colorScheme.onTertiary
+                    )
+                    else if(state.selected.contains(category)) Image(
+                        painterResource(R.drawable.enabled_check_box),
+                        (null), Modifier.size(32.dp)
+                    )
+                }
+                if(!category.children.isNullOrEmpty()
+                    && states.contains(index)
+                ) {
+                    Divider(); FlowLayout(
+                        Modifier
+                            .background(colorScheme.primaryContainer)
+                            .padding(top = 16.dp)
+                            .padding(horizontal = 8.dp), 8.dp, 8.dp
+                    ) {
+                        category.children?.let {
+                            it.forEach { sub ->
+                                GiltyChip(
+                                    Modifier, sub.name,
+                                    state.selected.contains(sub)
+                                ) { callback?.onSubClick(sub) }
+                            }
+                        }
                     }
                 }
             }
-            Buttons(
-                Modifier, { callback?.onDone() })
-            { callback?.onClear() }
         }
     }
 }
@@ -113,8 +188,8 @@ fun CategoryList(
 @Composable
 private fun Buttons(
     modifier: Modifier = Modifier,
-    Done: () -> Unit,
-    Clear: () -> Unit
+    onComplete: () -> Unit,
+    onClear: () -> Unit,
 ) {
     Column(modifier) {
         GradientButton(
@@ -122,7 +197,7 @@ private fun Buttons(
                 .padding(horizontal = 16.dp)
                 .padding(top = 28.dp),
             stringResource(R.string.meeting_filter_complete_button)
-        ) { Done() }
+        ) { onComplete() }
         Text(
             stringResource(R.string.meeting_filter_clear),
             Modifier
@@ -130,90 +205,11 @@ private fun Buttons(
                 .padding(top = 12.dp, bottom = 28.dp)
                 .clip(CircleShape)
                 .clickable(
-                    MutableInteractionSource(),
-                    (null)
-                ) { Clear() },
+                    MutableInteractionSource(), (null)
+                ) { onClear() },
             colorScheme.tertiary,
             textAlign = TextAlign.Center,
             style = typography.bodyLarge
         )
     }
 }
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-private fun Item(
-    category: CategoryModel,
-    select: Boolean,
-    subCategories: List<Pair<CategoryModel, String>>,
-    onSubSelect: (String) -> Unit,
-    onClick: () -> Unit,
-) {
-    Column {
-        Card(
-            onClick, Modifier
-                .fillMaxWidth()
-                .padding(vertical = 6.dp), true,
-            MaterialTheme.shapes.large,
-            CardDefaults.cardColors(colorScheme.primaryContainer)
-        ) {
-            Row(
-                Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp),
-                Arrangement.Absolute.SpaceBetween,
-                CenterVertically
-            ) {
-                Row {
-                    GEmojiImage(
-                        category.emoji,
-                        Modifier.size(20.dp)
-                    )
-                    Text(
-                        category.name, Modifier
-                            .padding(start = 18.dp),
-                        colorScheme.tertiary,
-                        style = typography.bodyMedium,
-                        fontWeight = SemiBold
-                    )
-                }
-                if(category.children != null) Icon(
-                    if(select) Filled.KeyboardArrowDown
-                    else Filled.KeyboardArrowRight,
-                    stringResource(R.string.next_button),
-                    tint = colorScheme.onTertiary
-                ) else if(select)
-                    Image(painterResource(enabled_check_box), (null))
-            }
-            category.children?.let { list ->
-                if(select) (SubCategories(category, list, subCategories)
-                { onSubSelect(it) })
-            }
-        }
-    }
-}
-
-@Composable
-private fun SubCategories(
-    category: CategoryModel,
-    subCategories: List<CategoryModel>,
-    selectBubCategories:
-    List<Pair<CategoryModel, String>>,
-    onSelect: (String) -> Unit
-) {
-    Divider()
-    FlowLayout(
-        Modifier
-            .background(colorScheme.primaryContainer)
-            .padding(top = 16.dp)
-            .padding(horizontal = 16.dp), 8.dp, 8.dp
-    ) {
-        subCategories.forEach {
-            GiltyChip(
-                Modifier, it.name, selectBubCategories
-                    .contains(Pair(category, it.name))
-            ) { onSelect(it.name) }
-        }
-    }
-}
-
