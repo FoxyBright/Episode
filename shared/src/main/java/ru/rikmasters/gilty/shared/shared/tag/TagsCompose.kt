1c93805ccc4c451536c9f257294bc975c0ba0cd7
Ivan Evstratov <i.evstratov@rikmasters.ru>
2023-04-18 12:24:40 +0300

    Добавлен плейсхолдер на экран поиска тегов

--- shared/src/main/java/ru/rikmasters/gilty/shared/shared/tag/TagsCompose.kt
+++ shared/src/main/java/ru/rikmasters/gilty/shared/shared/tag/TagsCompose.kt
@@ -4,12 +4,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight.Companion.SemiBold
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ru.rikmasters.gilty.shared.R
@@ -30,7 +32,7 @@ private fun TagSearchPreview() {
     GiltyTheme {
         Box(
             Modifier.background(
-                MaterialTheme.colorScheme.background
+                colorScheme.background
             )
         ) {
             TagsContent(
@@ -53,7 +55,7 @@ private fun TagSearchListPreview() {
     GiltyTheme {
         Box(
             Modifier.background(
-                MaterialTheme.colorScheme.background
+                colorScheme.background
             )
         ) {
             TagsContent(
@@ -140,7 +142,18 @@ private fun Content(
     modifier: Modifier,
     callback: TagsCallback?,
 ) {
-    Column(
+    if(state.search.isNotBlank()
+        && state.searchResult.isEmpty()
+    ) Box(Modifier.fillMaxSize()) {
+        Text(
+            stringResource(R.string.meeting_filter_tags_placeholder),
+            Modifier.align(Alignment.Center),
+            style = typography.bodyMedium,
+            fontWeight = SemiBold,
+            color = colorScheme.scrim
+        )
+    }
+    else Column(
         modifier
             .fillMaxWidth()
             .fillMaxHeight(0.88f)
