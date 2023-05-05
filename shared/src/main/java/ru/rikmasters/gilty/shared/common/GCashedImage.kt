d196e4440364a394094c3412a90786a79455c28c
Ivan Evstratov <i.evstratov@rikmasters.ru>
2023-05-05 12:40:24 +0300

    Кеширование фоток

--- shared/src/main/java/ru/rikmasters/gilty/shared/common/GCashedImage.kt
+++ shared/src/main/java/ru/rikmasters/gilty/shared/common/GCashedImage.kt
@@ -18,15 +18,10 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import coil.compose.AsyncImagePainter.State.Success
 import coil.compose.rememberAsyncImagePainter
-import coil.memory.MemoryCache
 import coil.request.CachePolicy.ENABLED
 import coil.request.ImageRequest
 import coil.size.Size.Companion.ORIGINAL
 import org.json.JSONObject
-import org.koin.androidx.compose.get
-import ru.rikmasters.gilty.core.env.Environment
-import ru.rikmasters.gilty.core.util.extension.slash
-import ru.rikmasters.gilty.data.ktor.Ktor.logE
 import ru.rikmasters.gilty.shared.theme.base.ThemeExtra.colors
 import kotlin.text.Charsets.UTF_8
 
@@ -41,38 +36,22 @@ fun GCashedImage(
     colorFilter: ColorFilter? = null,
 ) {
     val context = LocalContext.current
-    
-    val env = get<Environment>()
-    
-    val baseUrl: String = env["WEB_BASE_URL"]
-        ?: "gilty.rikmasters.ru/api/v1".also {
-            logE("Не задана переменная среды WEB_BASE_URL")
-        }
-    
-    val host = remember(baseUrl) {
-        parse(baseUrl).let {
-            val scheme = "http://"
-            val host = parse(scheme.plus(it)).host
-            scheme.plus(host)
-        }
-    }
+    val key = url?.getPhotoKey()
     
     val builder = key(url) {
         remember(url) {
-            val pair = getPhotoKey(host, url)
-            val builder = ImageRequest.Builder(context)
-                .data(pair?.first)
+            val builder = ImageRequest
+                .Builder(context)
+                .data(url)
                 .size(ORIGINAL)
                 .networkCachePolicy(ENABLED)
                 .diskCachePolicy(ENABLED)
                 .memoryCachePolicy(ENABLED)
                 .allowHardware(false)
-            pair?.let {
-                builder
-                    .memoryCacheKey(pair.memoryCacheKey())
-                    .diskCacheKey(pair.diskCacheKey())
-            } ?: run { builder.diskCacheKey(url).memoryCacheKey(url) }
-            
+            key?.let {
+                builder.memoryCacheKey(it)
+                builder.diskCacheKey(it)
+            }
             builder.build()
         }
     }
@@ -96,42 +75,20 @@ fun GCashedImage(
     )
 }
 
-private fun Pair<String, *>?.memoryCacheKey() = this?.run {
-    MemoryCache.Key(first, mapOf("extraKey" to second.toString()))
-}
-
-private fun Pair<String, *>?.diskCacheKey() = this?.run {
-    "$first&extraKey=${second}"
-}
-
-private fun getPhotoKey(
-    host: String,
-    url: String?,
-): Pair<String, Boolean>? {
-    return url?.let {
-        try {
-            val uri = parse(url)
-            
-            val hash = uri.getQueryParameter("hash")
-            
-            val policy = uri.getQueryParameter("policy")
-                ?: throw IllegalStateException("Нет поля policy в $url")
-            
-            val obj = Base64.decode(policy, DEFAULT).toString(UTF_8)
-            
-            val json = JSONObject(obj)
-            
-            val path = json.getString("path")
-            val blur = json.getBoolean("blur")
-            
-            (host slash path).plus("?policy=$policy&hash=$hash") to blur
-        } catch(e: Exception) {
-            e.stackTraceToString()
-            logE("Проблемы кэширования фото URL: $url")
-            null
+private fun String.getPhotoKey() = try {
+    parse(this)
+        .getQueryParameter("policy")
+        ?.let { policy ->
+            Base64
+                .decode(policy, DEFAULT)
+                .toString(UTF_8)
+                .let { JSONObject(it) }
+                .let {
+                    val path = it.getString("path")
+                    val blur = it.getBoolean("blur")
+                    "$path?blur=$blur"
+                }
         }
-    } ?: run {
-        logE("Проблемы кэширования фото URL: $url")
-        null
-    }
+} catch(e: Exception) {
+    null
 }
