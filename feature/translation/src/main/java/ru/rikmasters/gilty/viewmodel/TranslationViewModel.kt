34c963b1d686c4dbc89585888c4103ffbc6d2cdd
Yotfr <yotfr73@gmail.com>
2023-05-12 11:22:29 +0300

    translation ping

--- feature/translation/src/main/java/ru/rikmasters/gilty/viewmodel/TranslationViewModel.kt
+++ feature/translation/src/main/java/ru/rikmasters/gilty/viewmodel/TranslationViewModel.kt
@@ -1,15 +1,38 @@
 package ru.rikmasters.gilty.viewmodel
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.koin.core.component.inject
 import ru.rikmasters.gilty.core.viewmodel.ViewModel
 import ru.rikmasters.gilty.event.TranslationEvent
+import ru.rikmasters.gilty.shared.model.translations.TranslationInfoModel
 import ru.rikmasters.gilty.translations.repository.TranslationRepository
 
 class TranslationViewModel : ViewModel() {
 
     private val translationRepository: TranslationRepository by inject()
+
+    private val pinging = MutableStateFlow(false)
+
+    private val translationInfo = MutableStateFlow<TranslationInfoModel?>(null)
+
+    init {
+        coroutineScope.launch {
+            pinging.collectLatest { isPinging ->
+                translationInfo.value?.let { translation ->
+                    while (isPinging) {
+                        translationRepository.ping(
+                            translationId = translation.id
+                        )
+                        delay(8000)
+                    }
+                }
+            }
+        }
+    }
 
     fun onEvent(event: TranslationEvent) {
         when(event) {
@@ -18,6 +41,7 @@ class TranslationViewModel : ViewModel() {
                     translationRepository.connectToTranslation(
                         translationId = event.translationId
                     )
+                    startPinging()
                 }
             }
             is TranslationEvent.ConnectToTranslationChat -> {
@@ -30,6 +54,7 @@ class TranslationViewModel : ViewModel() {
             is TranslationEvent.DisconnectFromTranslation -> {
                 coroutineScope.launch {
                     translationRepository.disconnectFromTranslation()
+                    stopPinging()
                 }
             }
             is TranslationEvent.DisconnectFromTranslationChat -> {
@@ -38,6 +63,14 @@ class TranslationViewModel : ViewModel() {
                 }
             }
         }
+    }
+
+    private fun startPinging() {
+        pinging.value = true
+    }
+
+    private fun stopPinging() {
+        pinging.value = false
     }
 
 }
